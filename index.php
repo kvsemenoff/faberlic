5ce511ea33cd169242ce3140ba36fe99225d01c4
Konstantin Semenov <kvsemenoff@mail.ru>
2016-05-19 18:05:26 +0400

    s

--- index.php
+++ index.php
@@ -6,7 +6,9 @@
 	<title>Главная страница</title>
 	<link rel="shortcut icon" href="favicon.png" />
 	<link rel="stylesheet" href="css/style.css">
-	
+	<link rel="stylesheet" href="css/style2.css">
+	<link rel="stylesheet" href="css/style3.css">
+
 	<link rel="stylesheet" href="libs/bootstrap/bootstrap-grid-3.3.1.min.css" />
 	<link rel="stylesheet" href="libs/font-awesome-4.2.0/css/font-awesome.min.css" />
 	<link rel="stylesheet" href="libs/fancybox/jquery.fancybox.css" />

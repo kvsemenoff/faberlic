506c02484b8a4caa894af3c738208a75935bc737
Konstantin Semenov <kvsemenoff@mail.ru>
2016-05-20 11:22:26 +0400

    dddd

---
+++ futures.php
@@ -0,0 +1,48 @@
+<section class="futures">
+	<div class="container">
+		<div class="row">
+			<div class="col-md-4">
+				<div class="future">
+					<div class="futures__img-box">
+						<img src="img/fut1.png" alt="">
+					</div>		
+					<span class="futures__title">-Посчитайте свой ИМТ-</span>
+					<div class="futures__text">
+						Расчитайте свой индекс массы 
+						тела и выбирайте подходящую 
+						программу
+					</div>
+					<a href="#" class="button-1">Посчитать</a>
+				</div>
+			</div>
+			<div class="col-md-4">
+				<div class="future">
+					<div class="futures__img-box">
+						<img src="img/fut2.png" alt="">
+					</div>		
+					<span class="futures__title">-Выберите программу-</span>
+					<div class="futures__text">
+						Мы подготовили 3 программы
+						для разных людей и разных 
+						потребностей
+					</div>
+					<a href="#" class="button-1">Выбрать</a>
+				</div>
+			</div>
+			<div class="col-md-4">
+				<div class="future">
+					<div class="futures__img-box">
+						<img src="img/fut3.png" alt="">
+					</div>		
+					<span class="futures__title">-Узнайте о продуктах-</span>
+					<div class="futures__text">
+						Полезные продукты которые 
+						помогут вам управлять 
+						своим весом
+					</div>
+					<a href="#" class="button-1">Узнать</a>
+				</div>
+			</div>
+		</div>
+	</div>
+</section>

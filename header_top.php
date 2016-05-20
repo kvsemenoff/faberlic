aafd55e93cec4073ad9939c4e55ad544f0c33937
anzarsh <anzarsh@mail.ru>
2016-05-20 11:31:57 +0300

    header-top разметка

--- header_top.php
+++ header_top.php
@@ -1,20 +1,22 @@
 <section class="header-top">
 		<div class="container">
 			<div class="row">
-				<div class="col-md-2 col-sm-12 col-xs-12">
-					<a href="/"><img src="img/faberlic.png" alt=""></a>
-				</div>
-				<div class="col-md-7 col-sm-12 col-xs-12">
-					<ul class="header-top__menu">
-
+				<div class="col-xs-4 col-sm-4 col-md-4 col-lg-4 col-lg-push-4">
+					<span class="hidden-lg"><i class="fa fa-bars" aria-hidden="true"></i>меню</span>
+					<ul class="header-top__menu hidden-xs hidden-sm hidden-md">
 						<li class="menu-item"><a href="#" class="menu-item__link">ПРОГРАММЫ</a></li>
 						<li class="menu-item"><a href="#" class="menu-item__link">ПРОДУКЦИЯ</a></li>
 						<li class="menu-item"><a href="#" class="menu-item__link">ПОЛЕЗНЫЕ СОВЕТЫ</a></li>
 						<li class="menu-item"><a href="#" class="menu-item__link">ОТЗЫВЫ</a></li>
 					</ul>
 				</div>
-				<div class="col-md-3 col-sm-12 col-xs-12">
-					<a href="#" class="header-top__login">РЕГИСТРАЦИЯ В ПРОГРАММЕ</a>
+
+				<div class="col-xs-8 col-sm-4 col-md-4 col-lg-4 col-lg-pull-4">
+					<a href="/"><img src="img/faberlic-xs.png" alt=""></a>
+				</div>
+				
+				<div class="hidden-xs col-sm-4 col-md-4 col-lg-4">
+					<a href="#" class="header-top__login">РЕГИСТРАЦИЯ <span class="fab-hid-sm fab-vis-md">В ПРОГРАММЕ</span></a>
 				</div>
 			</div>
 			<dic class="cl"></dic><dic class="cl"></dic>

103e3cd8522ccd881cdad43bd0b0a3dc753e9887
hatuhov <hatuhov@yandex.ru>
2016-05-21 11:55:07 +0400

    programs.php

--- programs.php
+++ programs.php
@@ -37,6 +37,28 @@
 
 <!-- *********************** programs.php *********************** -->
 	
+<!-- *********************** header_middle ***********************-->
+	<section class="header_middle">
+		<div class="container">
+			<div class="row">
+				<div class="col-md-7 col-sm-6 col-xs-12">
+					<div class="header_middle-left">
+						<span class="header_middle-span1 header_middle-span1__programs">Программы</span>
+						<p class="header_middle-span3 header_middle-span3__programs">
+							Все люди разные и у всех разные потребности в коррекции веса – кому-то нужно кардинально поменять себя, а кому-то достаточно просто поддерживать себя в форме. Для этого мы разработали 3 программы Управления весом для разных людей и разных потребностей
+						</p>
+					</div>
+				</div>
+				<div class="col-md-5 col-sm-6 col-xs-12">
+					<div class="header_middle-right">
+						<img src="img/program-girl-top.png" alt="program-girl-top" class="header_middle-women header_middle-women__programs">
+					</div>
+				</div>
+			</div>
+		</div>
+	</section>
+<!-- *********************** header_middle ***********************-->
+<div class="program-cell">
 	<div class="program_item">
 		<div class="program_item__img-box">
 			<img src="img/program-img.png" alt="program-img" class="program_item__imgtop">
@@ -49,7 +71,36 @@
 		</div>
 		<div class="program_item-button"></div>  <!-- Указать существующий класс кнопки -->
 	</div>
-	
+</div>
+<div class="program-cell program-cell1">
+	<div class="program_item program_item1">
+		<div class="program_item__img-box">
+			<img src="img/program-img1.png" alt="program-img1" class="program_item__imgtop">
+		</div>
+		<div class="program_item__title">Ускорение</div>
+		<div class="program_item__subtitle">интенсивная программа</div>
+		<div class="program_item__text">Для тех, кто хочет получить быстрый результат и готов активно работать над собой</div>
+		<div class="program_item__girl">
+			<img src="img/tetka.png" alt="tetka" class="program_item__girl-img1">
+		</div>
+		<div class="program_item-button"></div>  <!-- Указать существующий класс кнопки -->
+	</div>
+</div>
+<div class="program-cell program-cell2">
+	<div class="program_item program_item2">
+		<div class="program_item__img-box">
+			<img src="img/program-img2.png" alt="program-img2" class="program_item__imgtop">
+		</div>
+		<div class="program_item__title">Разгрузочный день</div>
+		<div class="program_item__subtitle">Периодическая программа</div>
+		<div class="program_item__text">Для тех, кто доволен своим весом, но иногда хочет избавиться от чувства тяжести</div>
+		<div class="program_item__girl">
+			<img src="img/program-girl1.png" alt="program-girl1" class="program_item__girl-img2">
+		</div>
+		<div class="program_item-button"></div>  <!-- Указать существующий класс кнопки -->
+	</div>
+</div>
+<a href="#" class="program_item-link">Подбери себе программу</a>
 <!-- *********************** programs.php *********************** -->
 
 	<?php require_once('footer.php'); ?>

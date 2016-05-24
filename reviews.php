e5a3c5ebcf4bc79135204f867c81c9d4863080a1
hatuhov <hatuhov@yandex.ru>
2016-05-24 12:59:13 +0400

    240516

---
+++ reviews.php
@@ -0,0 +1,79 @@
+﻿<!doctype html>
+<html lang="ru">
+<head>
+	<meta charset="UTF-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<title>Полезные советы</title>
+	<link rel="shortcut icon" href="favicon.png" />
+	<link rel="stylesheet" href="css/style.css">
+	<link rel="stylesheet" href="css/style2.css">
+	<link rel="stylesheet" href="css/style3.css">
+
+	<link rel="stylesheet" href="css/fonts.css">
+
+	<link rel="stylesheet" href="libs/bootstrap/bootstrap-grid-3.3.1.min.css" />
+	<link rel="stylesheet" href="libs/font-awesome-4.2.0/css/font-awesome.min.css" />
+	<link rel="stylesheet" href="libs/fancybox/jquery.fancybox.css" />
+	
+	
+	<link rel="stylesheet" href="libs/owl.carousel/assets/owl.carousel.css" />
+	<link rel="stylesheet" href="libs/wow/animate.css">
+	<meta content="telephone=no" name="format-detection"/>
+</head>
+<body>
+	<script src="libs/jquery/jquery-1.11.1.min.js"></script>
+	<script src="libs/owl.carousel/owl.carousel.js"></script>
+	<script src="libs/fancybox/jquery.fancybox.pack.js"></script>
+	<script src="libs/wow/wow.min.js"></script>
+
+	<script>
+    	new WOW().init();
+    </script>
+	<script src="js/jquery.maskedinput.min.js"></script>
+	<script src="js/parallax.min.js"></script>
+	<script src="js/common.js"></script>
+
+	<?php require_once('header_top.php'); ?>
+
+<!-- *********************** soveti.php *********************** -->
+	
+	<section class="header_middle">
+		<div class="container">
+			<div class="row">
+				<div class="col-md-7 col-sm-6 col-xs-12">
+					<div class="header_middle-left">
+						<span class="header_middle-span1 header_middle-span1__programs">Полезные советы</span>
+						<span class="header_middle-span3 header_middle-span3__programs">В основе любой программы – базовые принципы сбалансированного питания</span>
+					</div>
+				</div>
+				<div class="col-md-5 col-sm-6 col-xs-12">
+					<div class="header_middle-right">
+						<img src="img/soveti-women.png" alt="program-girl-top" class="header_middle-women header_middle-women__programs">
+					</div>
+				</div>
+			</div>
+		</div>
+	</section>
+	<section class="soveti_middle">
+		<div class="container">
+			<div class="row">
+				<div class="col-md-6 col-sm-6 col-xs-12 span-option-cell">
+					<img src="img/soveti-img1.png" alt="soveti-img1">
+					<span class="ac-soveti-span-title">- водный баланс -</span>
+					<span class="ac-soveti-span">Вес указанный в анкете : 115 кг</span>
+					<span class="ac-soveti-span">Последний вес (по дневнику ): 98 кг</span>
+					<p class="ac-soveti-span">Доброго времени суток хотелось поблагодарить за удивительную программу которая помогает измениться, очень многие моменты  продуманны от каш из льна до удивительных продуктов как итлес и фитокс. </p>
+					<a href="#" class="ac-soveti-link"><span class="ac-soveti-span"><span class="ac-soveti-img"></span>Читать подробнее</span></a>
+				</div>
+			</div>
+		</div>
+	</section>
+	
+<!-- *********************** soveti.php *********************** -->
+
+	<?php require_once('footer.php'); ?>
+	
+
+	
+</body>
+</html>

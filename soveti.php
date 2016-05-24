d7ece30af25d2c694a3fa6394419bd7629cff28c
hatuhov <hatuhov@yandex.ru>
2016-05-24 16:25:05 +0400

    24052016

--- soveti.php
+++ soveti.php
@@ -43,7 +43,7 @@
 				<div class="col-md-7 col-sm-6 col-xs-12">
 					<div class="header_middle-left">
 						<span class="header_middle-span1 header_middle-span1__programs">Полезные советы</span>
-						<span class="header_middle-span3 header_middle-span3__programs">В основе любой программы – базовые принципы сбалансированного питания</span>
+						<span class="header_middle-span3 header_middle-span3__soveti">В основе любой программы – базовые принципы сбалансированного питания</span>
 					</div>
 				</div>
 				<div class="col-md-5 col-sm-6 col-xs-12">
@@ -57,25 +57,33 @@
 	<section class="soveti_middle">
 		<div class="container">
 			<div class="row">
-				<div class="col-md-6 col-sm-6 col-xs-12 span-option-cell">
-					<img src="img/soveti-img1.png" alt="soveti-img1">
-					<span class="ac-soveti-span-title">- водный баланс -</span>
-					<span class="ac-soveti-span">За день необходимо выпивать не менее 1,5 л чистой воды.</span>
+				<div class="col-md-6 col-sm-6 col-xs-12">
+					<div class="ac-soveti-cell">
+						<img src="img/soveti-img1.png" alt="soveti-img1" class="ac-soveti-img">
+						<span class="ac-soveti-span-title">- водный баланс -</span>
+						<span class="ac-soveti-span">За день необходимо выпивать не менее 1,5 л чистой воды.</span>
+					</div>
 				</div>
-				<div class="col-md-6 col-sm-6 col-xs-12 span-option-cell">
-					<img src="img/soveti-img2.png" alt="soveti-img2">
-					<span class="ac-soveti-span-title">- режим питания -</span>
-					<span class="ac-soveti-span">Питаться нужно не менее 5 раз в день, но небольшими порциями.</span>
+				<div class="col-md-6 col-sm-6 col-xs-12">
+					<div class="ac-soveti-cell">
+						<img src="img/soveti-img2.png" alt="soveti-img2" class="ac-soveti-img">
+						<span class="ac-soveti-span-title">- режим питания -</span>
+						<span class="ac-soveti-span">Питаться нужно не менее 5 раз в день, но небольшими порциями.</span>
+					</div>
 				</div>
-				<div class="col-md-6 col-sm-6 col-xs-12 span-option-cell">
-					<img src="img/soveti-img3.png" alt="soveti-img3">
-					<span class="ac-soveti-span-title">- Сбалансированный рацион -</span>
-					<p class="ac-soveti-span">Исключить: газированные сладкие напитки, фаст-фуд, жирные полуфабрикаты и выпечку. Включить: хотя бы в один прием пищи продукты, богатые белком и пищевыми волокнами (например, свежие овощи или вареная куриная грудка).</p>
+				<div class="col-md-6 col-sm-6 col-xs-12">
+					<div class="ac-soveti-cell">
+						<img src="img/soveti-img3.png" alt="soveti-img3" class="ac-soveti-img">
+						<span class="ac-soveti-span-title">- Сбалансированный рацион -</span>
+						<p class="ac-soveti-span">Исключить: газированные сладкие напитки, фаст-фуд, жирные полуфабрикаты и выпечку. Включить: хотя бы в один прием пищи продукты, богатые белком и пищевыми волокнами (например, свежие овощи или вареная куриная грудка).</p>
+					</div>
 				</div>
-				<div class="col-md-6 col-sm-6 col-xs-12 span-option-cell">
-					<img src="img/soveti-img4.png" alt="soveti-img4">
-					<span class="ac-soveti-span-title">- физические нагрузки -</span>
-					<p class="ac-soveti-span">Для достижения максимальной эффективности Программы не забывайте про физическую нагрузку – постарайтесь 2-3 раза в неделю заниматься в тренажерном зале или ходить в бассейн, или хотя бы начинайте каждое утро с простой зарядки или не менее 30 минут ходите пешком.</p>
+				<div class="col-md-6 col-sm-6 col-xs-12">
+					<div class="ac-soveti-cell">
+						<img src="img/soveti-img4.png" alt="soveti-img4" class="ac-soveti-img">
+						<span class="ac-soveti-span-title">- физические нагрузки -</span>
+						<p class="ac-soveti-span">Для достижения максимальной эффективности Программы не забывайте про физическую нагрузку – постарайтесь 2-3 раза в неделю заниматься в тренажерном зале или ходить в бассейн, или хотя бы начинайте каждое утро с простой зарядки или не менее 30 минут ходите пешком.</p>
+					</div>
 				</div>
 			</div>
 		</div>

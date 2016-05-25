cfb27f56e0b0e0eb8c4adca72682ea70589aa631
anzarsh <anzarsh@mail.ru>
2016-05-25 13:41:55 +0300

    доработки top-header и страницы со шпинатом

--- header_top.php
+++ header_top.php
@@ -1,9 +1,12 @@
 <section class="header-top">
 		<div class="container">
 			<div class="row">
-				<div class="col-xs-4 col-sm-4 col-md-4 col-lg-6 col-lg-push-3 padding-left">
+				<div class="col-xs-6 col-sm-2 col-md-6 col-lg-7 col-md-push-2">
 					<div class="header-top__menu-unpressed">
-						<span class="menu-menu hidden-lg"><i class="fa fa-bars" aria-hidden="true"></i> меню</span>
+						<span class="menu-menu hidden-md hidden-lg">
+							<span id="icon1" class="ae-dispnone"><i class="fa fa-times" aria-hidden="true"></i></span>
+							<span id="icon2" class=""><i class="fa fa-bars" aria-hidden="true"></i></span> меню
+						</span>
 						<ul class="menu-list-unpressed">
 							<li class="menu-item"><a href="#" class="menu-item__link">ПРОГРАММЫ</a></li>
 							<li class="menu-item"><a href="#" class="menu-item__link">ПРОДУКЦИЯ</a></li>
@@ -13,12 +16,16 @@
 					</div>
 				</div>
 
-				<div class="col-xs-8 col-sm-4 col-md-4 col-lg-3 col-lg-pull-6">
-					<a href="/" class="header-top__logo"><img src="img/faberlic-xs.png" alt=""></a>
+				<div class="col-xs-6 col-sm-8 col-md-2 col-lg-2 col-md-pull-6">
+					<div class="logobox">
+						<a href="/" class=""><img src="img/faberlic.png" alt=""></a>
+					</div>
 				</div>
 				
-				<div class="hidden-xs col-sm-4 col-md-4 col-lg-3">
+				<div class="hidden-xs col-sm-2 col-md-4 col-lg-3">
+				<div class="register-box">
 					<a href="#" class="header-top__login">РЕГИСТРАЦИЯ <span class="fab-hid-sm fab-vis-md">В ПРОГРАММЕ</span></a>
+					</div>
 				</div>
 			</div>
 		</div>
